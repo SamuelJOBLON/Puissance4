0aeff2c1e6810121e5ad9a43928a63c705eb7d43
SamuelJOBLON <samuel.joblon@gmail.com>
2018-10-17 16:32:03 +0200

    test

--- Puissance4.java
+++ Puissance4.java
@@ -3,20 +3,26 @@ import java.util.Scanner;
 public class Puissance4 {
 
   static Scanner saisie;
+  static int largeur = 7;
+  static int hauteur = 5;
+  static String[][] grille = new String[largeur][hauteur];
 
   public static void main (String[] args) {
 
     saisie = new Scanner(System.in);
 
-    int largeur = 7;
-    int hauteur = 5;
-    String[][] grille = new String[largeur][hauteur];
+
 
     for (int col = 0; col < largeur; col++) {
       System.out.print("  " + (col+1) + " ");
     }
     System.out.println();
-    // System.out.println(Reset(grille));
+    Reset(grille);
+
+  }
+
+  public static String[][] Reset(String[][] grille) {
+
     for (int row = 0; row < hauteur; row++) {
       for (int col = 0; col < largeur; col++) {
         grille[col][row] = ".";
@@ -31,31 +37,10 @@ public class Puissance4 {
       }
 
       System.out.println();
-
     }
 
-  }
+    return grille;
 
-  // public static String[][] Reset(String[][] grille) {
-  //
-  //   for (int row = 0; row < hauteur; row++) {
-  //     for (int col = 0; col < largeur; col++) {
-  //       grille[col][row] = ".";
-  //
-  //       if (col == largeur - 1) {
-  //         System.out.print("| " + grille[col][row] + " |");
-  //       }
-  //       else {
-  //         System.out.print("| " + grille[col][row] + " ");
-  //       }
-  //
-  //     }
-  //
-  //     System.out.println();
-  //   }
-  //
-  //   return grille;
-  //
-  // }
+  }
 
 }
