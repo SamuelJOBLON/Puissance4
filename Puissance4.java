a8b7d3360cb5ff8b89f418bfe6499869fafa9a42
SamuelJOBLON <samuel.joblon@gmail.com>
2018-10-17 17:14:22 +0200

    ok

--- Puissance4.java
+++ Puissance4.java
@@ -6,26 +6,39 @@ public class Puissance4 {
   static int largeur = 7;
   static int hauteur = 5;
   static String[][] grille = new String[largeur][hauteur];
+  static String joueurUn;
+  static String joueurDeux;
+  static String name;
 
   public static void main (String[] args) {
 
     saisie = new Scanner(System.in);
 
+    NomsDesJoueurs();
+    Reset(grille);
+    AffichageGrille(grille);
 
+  }
 
+  public static String NomsDesJoueurs() {
+    System.out.println();
+    System.out.println("    **** ! BIENVENUE SUR PUISSANCE 4 !  ****");
+    System.out.println();
+    System.out.print("Joueur 1 (entrez votre nom) : ");
+    joueurUn = saisie.nextLine();
+    System.out.print("Joueur 2 (entrez votre nom) : ");
+    joueurDeux = saisie.nextLine();
+
+    return name;
+  }
+
+  public static String[][] AffichageGrille(String[][] grille) {
     for (int col = 0; col < largeur; col++) {
       System.out.print("  " + (col+1) + " ");
     }
     System.out.println();
-    Reset(grille);
-
-  }
-
-  public static String[][] Reset(String[][] grille) {
-
     for (int row = 0; row < hauteur; row++) {
       for (int col = 0; col < largeur; col++) {
-        grille[col][row] = ".";
 
         if (col == largeur - 1) {
           System.out.print("| " + grille[col][row] + " |");
@@ -40,7 +53,19 @@ public class Puissance4 {
     }
 
     return grille;
+  }
 
+  public static String[][] Reset(String[][] grille) {
+
+    for (int row = 0; row < hauteur; row++) {
+      for (int col = 0; col < largeur; col++) {
+        grille[col][row] = ".";
+
+      }
+
+    }
+
+    return grille;
   }
 
 }
