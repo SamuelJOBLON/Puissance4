c33a80566ed0b234a58f8baf3c445081a79a60ff
SamuelJOBLON <samuel.joblon@gmail.com>
2018-10-17 15:06:41 +0200

    init

--- Puissance4.java
+++ Puissance4.java
@@ -1,1 +1,13 @@
-// Ce jeu est le Puissance 4
+import java.util.Scanner;
+
+public class Puissance4 {
+
+  static Scanner saisie;
+
+  public static void main (String[] args) {
+
+    saisie = new Scanner(System.in);
+
+  }
+
+}
