cd0d16213696152243a0e77ca77ca32faba9f074
SamuelJOBLON <samuel.joblon@gmail.com>
2018-11-12 09:47:10 +0100

    FINISH

--- Puissance4.java
+++ Puissance4.java
@@ -8,7 +8,7 @@ public class Puissance4 {
   static int largeur = 7;
   static int hauteur = 6;
   static int compteur = 0;
-  static int i = 0;
+  static int i, j;
   static int choix, row, col, colonne, tour, tourSuivant, temp, ligneResultante, rang;
   static char[][] grille = new char[largeur][hauteur];
   static char[][] grilleUpdated = new char[largeur][hauteur];
@@ -32,45 +32,31 @@ public class Puissance4 {
     System.out.println();
 
     while (compteur != largeur*hauteur &&
-            !ScanHorizontal(grilleUpdated, ligneResultante, pion) &&
-            !ScanVertical(grilleUpdated, colonne, pion)) {
+           !ScanHorizontal(grilleUpdated, colonne, ligneResultante, pion) &&
+           !ScanVertical(grilleUpdated, colonne, ligneResultante, pion) &&
+           !ScanDiagonalHaut(grilleUpdated, colonne, ligneResultante, pion) &&
+           !ScanDiagonalBas(grilleUpdated, colonne, ligneResultante, pion)) {
       colonne = ChoixColonne(grille, joueur);
       InsertPion(grilleUpdated, colonne, joueur, pion);
       System.out.println();
       AffichageGrille(grilleUpdated);
       System.out.println();
       System.out.println();
-      ScanHorizontal(grilleUpdated, ligneResultante, pion);
-      ScanVertical(grilleUpdated, colonne, pion);
+      ScanHorizontal(grilleUpdated, colonne, ligneResultante, pion);
+      ScanVertical(grilleUpdated, colonne, ligneResultante, pion);
+      ScanDiagonalHaut(grilleUpdated, colonne, ligneResultante, pion);
+      ScanDiagonalBas(grilleUpdated, colonne, ligneResultante, pion);
       tourSuivant = ChangementDeJoueur(tour);
       compteur += 1;
-      System.out.println(compteur);
       joueur = Joueur(tourSuivant, joueurUn, joueurDeux);
       pion = PionJoueur(joueur);
 
-      // System.out.println(ConditionsVictoire(grilleUpdated, joueurUn, joueurDeux));
-      // ConditionsVictoire(grilleUpdated, joueurUn, joueurDeux);
     }
 
     if (compteur == largeur*hauteur) {
       System.out.println();
       System.out.println("**** !!!!  GRILLE PLEINE  !!!! ****");
     }
-    // if (ScanHorizontal(grilleUpdated, ligneResultante, pion)) {
-    //   tourSuivant = ChangementDeJoueur(tour);
-    //   joueur = Joueur(tourSuivant, joueurUn, joueurDeux);
-    //   pion = PionJoueur(joueur);
-    //   rang = hauteur - ligneResultante;
-    //   System.out.println("il y a 4 " + pion + " à l'horizontale au rang " + rang);
-    //   System.out.println(joueur + " EST VAINQUEUR");
-    // }
-    // if (ScanVertical(grilleUpdated, colonne, pion)) {
-    //   tourSuivant = ChangementDeJoueur(tour);
-    //   joueur = Joueur(tourSuivant, joueurUn, joueurDeux);
-    //   pion = PionJoueur(joueur);
-    //   System.out.println("il y a 4 " + pion + " à la verticale à la colonne " + colonne);
-    //   System.out.println(joueur + " EST VAINQUEUR");
-    // }
 
   }
 
@@ -84,7 +70,6 @@ public class Puissance4 {
     joueurUn = saisie.nextLine();
     System.out.print("Joueur 2 (entrez votre nom) : ");
     joueurDeux = saisie.nextLine();
-
     return name;
   }
 
@@ -94,7 +79,6 @@ public class Puissance4 {
     for (col = 0; col < largeur; col++) {
       System.out.print("  " + (col+1) + " ");
     }
-
     System.out.println();
 
     // Ligne vides de séparateurs
@@ -107,58 +91,47 @@ public class Puissance4 {
         System.out.print("|   ");
       }
     }
-
     System.out.println();
 
     // Grille
     //-----------------------------------------------
     for (row = 0; row < hauteur; row++) {
       for (col = 0; col < largeur; col++) {
-
         if (col == largeur - 1) {
           System.out.print("| " + grille[col][row] + " |");
         }
         else {
           System.out.print("| " + grille[col][row] + " ");
         }
-
       }
-
       System.out.println();
     }
-
     return grille;
   }
 
   public static int TirageAuSort() {
     int nombreAleatoire = ((int) (Math.random() * (100-0))%2);
-
     return nombreAleatoire;
-
   }
 
   public static String Joueur(int tour, String nomUn, String nomDeux) {
-
     if (tour%2 == 0) {
       name = nomUn;
     }
     else {
       name = nomDeux;
     }
-
     return name;
-
   }
 
   public static int ChangementDeJoueur(int tourPrecedent) {
-
     tour += 1;
     return tour;
-
   }
 
   public static int ChoixColonne (char[][] grilleUpdated, String nomJoueur) {
-
+    // Test si la valeur entrée est bien un nombreAleatoire
+    //-----------------------------------------------------
     boolean isEntier = true;
     do {
       System.out.println();
@@ -174,9 +147,14 @@ public class Puissance4 {
       }
     }
     while (isEntier != true);
+
+    // On converti en valeur réelle du tableau 0 et 6
+    // Oui, j'ai trouvé plus sympas de présenter une grille qui présente les colonnes de 1 à 7 plutôt que de 0 à 6
+    //------------------------------------------------------------------------------------------------------------
     choix = temp - 1;
 
-
+    // Test si la valeur entrée est comprise entre 1 et 7 en visuel
+    //-------------------------------------------------------------
     if (choix < 0 || 6 < choix) {
       do {
         do {
@@ -198,33 +176,7 @@ public class Puissance4 {
       }
       while (choix < 0 || 6 < choix);
     }
-
-
-    if (grilleUpdated[choix][0] != '.') {
-      do {
-
-            System.out.println();
-            System.out.print("Attention " + nomJoueur + " ! La colonne est pleine, choisis-en une autre :  ");
-            temp = saisie.nextInt();
-            choix = temp - 1;
-
-            if (choix < 0 || 6 < choix) {
-              do {
-                System.out.println();
-                System.out.print("Attention " + nomJoueur + " ! choisis un chiffre entre 1 et 7 :  ");
-                temp = saisie.nextInt();
-                choix = temp - 1;
-              }
-              while (choix < 0 || 6 < choix);
-            }
-
-      }
-      while (grilleUpdated[choix][0] != '.');
-
-    }
-
     return choix;
-
   }
 
   public static char PionJoueur(String nomJoueur) {
@@ -234,17 +186,14 @@ public class Puissance4 {
     else {
       pion = 'X';
     }
-
     return pion;
   }
 
   public static char[][] InsertPion(char[][] grilleUpdated, int colonne, String nomJoueur, char pionJoueur) {
-
     // Reset row
     row = 0;
 
     while (grilleUpdated[choix][row] == '.' && row < hauteur-1) {
-
       row += 1;
     }
     if (grilleUpdated[choix][row] != '.') {
@@ -255,21 +204,19 @@ public class Puissance4 {
       grilleUpdated[choix][row] = pionJoueur;
       ligneResultante = row;
     }
-
     return grilleUpdated;
-
   }
 
-  public static boolean ScanHorizontal(char[][] grilleUpdated, int rowInit, char pionJoueur) {
+  public static boolean ScanHorizontal(char[][] grilleUpdated, int colInit, int rowInit, char pionJoueur) {
 
-    // contrôle de la condition horizontale
-    for (col = 0; col < largeur-1; col ++) {
+    i = 0;
+    rang = hauteur - rowInit;
+    for (col = 0; col < largeur; col++) {
       if (grilleUpdated[col][rowInit] == pionJoueur) {
-        i += 1; // compteur qui à la hauteur de 4 (4 pions alignés) déclare un vainqueur
+        i +=1;
         if (i == 4) {
           result = true;
-          rang = hauteur - rowInit;
-          System.out.println("il y a 4 " + pion + " a l'horizontale au rang " + rang);
+          System.out.println("il y a 4 " + pionJoueur + " a l'horizontale au rang " + rang);
           System.out.println(joueur + " EST VAINQUEUR");
         }
       }
@@ -277,20 +224,19 @@ public class Puissance4 {
         i = 0;
       }
     }
-
     return result;
   }
 
-  public static boolean ScanVertical(char[][] grilleUpdated, int colInit, char pionJoueur) {
+  public static boolean ScanVertical(char[][] grilleUpdated, int colInit, int rowInit, char pionJoueur) {
 
-    // contrôle de la condition horizontale
-    for (row = hauteur-1; 0 < row; row --) {
+    i = 0;
+    for (row = 0; row < hauteur; row++) {
       if (grilleUpdated[colInit][row] == pionJoueur) {
-        i += 1; // compteur qui à la hauteur de 4 (4 pions alignés) déclare un vainqueur
+        i +=1;
         if (i == 4) {
           result = true;
-          colonne += 1;
-          System.out.println("il y a 4 " + pion + " a la verticale a la colonne " + colonne);
+          colonne = colInit + 1;
+          System.out.println("il y a 4 " + pionJoueur + " a la verticale a la colonne " + colonne);
           System.out.println(joueur + " EST VAINQUEUR");
         }
       }
@@ -298,18 +244,76 @@ public class Puissance4 {
         i = 0;
       }
     }
-
     return result;
+
+  }
+
+  public static boolean ScanDiagonalHaut(char[][] grilleUpdated, int colInit, int rowInit, char pionJoueur) {
+
+    i = 0;
+    for (row = hauteur-1; 0 < row; row--) {
+      for (col = 0; col < largeur; col++) {
+        if (grilleUpdated[col][row] == pionJoueur) {
+          i += 1;
+          int a = largeur - col - 1;
+          int b = row;
+          for (j = 1; j <= Math.min(a, b); j++) {
+            if (grilleUpdated[col+j][row-j] == pionJoueur) {
+              i += 1;
+              if (i == 4) {
+                result = true;
+                System.out.println("il y a 4 " + pionJoueur + " en diagonale haut");
+                System.out.println(joueur + " EST VAINQUEUR");
+              }
+            }
+            else {
+              i = 0;
+            }
+          }
+        }
+        i = 0;
+      }
+    }
+    return result;
+
+  }
+
+  public static boolean ScanDiagonalBas(char[][] grilleUpdated, int colInit, int rowInit, char pionJoueur) {
+
+    i = 0;
+    for (row = 0; row < hauteur; row++) {
+      for (col = 0; col < largeur; col++) {
+        if (grilleUpdated[col][row] == pionJoueur) {
+          i += 1;
+          int a = largeur - col - 1;
+          int b = hauteur - row - 1;
+          for (j = 1; j <= Math.min(a, b); j++) {
+            if (grilleUpdated[col+j][row+j] == pionJoueur) {
+              i += 1;
+              if (i == 4) {
+                result = true;
+                System.out.println("il y a 4 " + pionJoueur + " en diagonale bas");
+                System.out.println(joueur + " EST VAINQUEUR");
+              }
+            }
+            else {
+              i = 0;
+            }
+          }
+        }
+        i = 0;
+      }
+    }
+    return result;
+
   }
 
   public static char[][] Reset(char[][] grille) {
-
     for (row = 0; row < hauteur; row++) {
       for (col = 0; col < largeur; col++) {
         grille[col][row] = '.';
       }
     }
-
     return grille;
   }
 
